74c1792db0595fb95d6b360c1bf34220c03c6dab
Dan Näsman <dan.nasman@gmail.com>
2018-09-12 21:25:10 +0300

    added index2.html, changed chat.js to use the new indexes properly

--- client/javascripts/chat.js
+++ client/javascripts/chat.js
@@ -1,26 +1,24 @@
-var name = "";
+var name;
 
 main = function () {
     'use strict';
     console.log("haloo")
-    getUserInterface();
-}
-
-getUserInterface = function () {
-    if (name === "") {
-        $(".inputName").append("<input type='text' id='nickname'><br>");
-        $(".inputName").append("<button type='button' id='nicknameButton'>ready</button>");
-        $("#nicknameButton").click(function () {
-            name = $("#nickname").val();
+    $("#nicknameButton").click(function () {
+        name = $("#nickname").val()
+        console.log(name);
+        if (name !== "") {
             console.log(name);
-        });
+            document.location = "http://localhost:3000/index2.html";
+            console.log(document.location);
+        }
+    });
+    if(name !== "") {
+        getMessageBox();
     }
-    getMessages();
-    getMessageBox();
 }
 
 getMessages = function () {
-    $(".messages").empty();
+    $("#messages").empty();
     $.getJSON("messages.json", function (foundMessages) {
         console.log(foundMessages);
         var allMessages = foundMessages.map(function (foundMessage) {
@@ -28,25 +26,22 @@ getMessages = function () {
         });
         allMessages.forEach(function (nickName) {
             var feed = nickName;
-            $(".messages").append("<div>" + feed);
+            $("#messages").append("<div>" + feed);
         });
     });
 }
 
 getMessageBox = function () {
-    $(".inputMessage").append("<input type='text' id='send'><br>");
-    $(".inputMessage").append("<button type='button' id='sendButton'>send</button>");
+    $("#inputMessage").append("<input type='text' id='send'><br>");
+    $("#inputMessage").append("<button href='index2.html' type='button' id='sendButton'>send</button>");
     $("#sendButton").click(function () {
-        if (name !== "") {
-            newMessage = $("#send").val();
-            console.log(newMessage);
-            if (newMessage !== "") {
-                messageObject = { "nickName": name, "message": newMessage };
-                $.post("messages", messageObject)
-                getMessages();
-            }
+        newMessage = $("#send").val();
+        console.log(newMessage);
+        if (newMessage !== "") {
+            messageObject = { "nickName": name, "message": newMessage };
+            $.post("messages", messageObject)
+            getMessages();
         }
-
     })
 
 }
